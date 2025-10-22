eeafa0bdfae84a8fdd6f0a01505df2ad681b9857
Matteo Piscitelli <matteo@matteopiscitelli.it>
2025-10-22 18:17:41 +0200

    Add NetworkManager config to disable wifi adapter power saving.

--- stage2/99-kosmos-setup/00-run.sh
+++ stage2/99-kosmos-setup/00-run.sh
@@ -11,6 +11,9 @@ install -m 644 files/kosmos.gpg.asc	            "${ROOTFS_DIR}/etc/apt/keyrings/
 install -m 755 -d                               "${ROOTFS_DIR}/etc/NetworkManager/system-connections/"
 install -m 600 files/SelfHotspot.nmconnection   "${ROOTFS_DIR}/etc/NetworkManager/system-connections/"
 
+install -m 755 -d                               "${ROOTFS_DIR}/etc/NetworkManager/conf.d/"
+install -m 644 wifi-powersave-off.conf          "${ROOTFS_DIR}/etc/NetworkManager/conf.d/"
+
 # Install KosmOS setup first-boot service
 install -m 755 -d                               "${ROOTFS_DIR}/usr/local/bin"
 install -m 755 files/kosmos-setup.sh            "${ROOTFS_DIR}/usr/local/bin/"
